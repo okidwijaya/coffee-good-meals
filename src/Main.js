28b4d99ebabe6bba788abde01ab5a7aa0de6cea3
okidwijaya <okidwijaya@gmail.com>
2022-01-27 23:14:26 +0700

    adding product page

--- src/Main.js
+++ src/Main.js
@@ -4,6 +4,7 @@ import Footer from "./components/footer";
 import Signup from "./pages/starter/Signup";
 import Login from "./pages/starter/Login";
 import Forgotpassword from "./pages/starter/ForgotPassword";
+import Product from "./pages/product/Product";
 
 function App() {
   return (
@@ -12,6 +13,20 @@ function App() {
         <Route path="/register" element={<Signup />}/>
         <Route path="/login" element={<Login />}/>
         <Route path="/forgotpassword" element={<Forgotpassword />}/>
+        <Route path="/products" element={<Product />}/> 
+{/* 
+        <Route path="products" element={<Products />}>
+          <Route path="search" element={<Search />} />
+          <Route path="favourite" element={< />} />
+          <Route path="coffee" element={< />} />
+          <Route path="noncoffee" element={< />} />
+          <Route path="foods" element={< />} />
+          <Route path=":id" element={<ProductDisplay />} />
+          
+          <Route path="add" element={<AddProduct />} />
+          <Route path="addCoupon" element={<AddProduct />} />
+        </Route> */}
+
       </Routes>
       <Footer />
     </Router>
