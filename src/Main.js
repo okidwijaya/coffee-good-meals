19e55976029d041e0c310442b218f11991566865
okidwijaya <okidwijaya@gmail.com>
2022-01-27 12:12:46 +0700

    adding forgot password page

--- src/Main.js
+++ src/Main.js
@@ -3,14 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; //Nav
 import Footer from "./components/footer";
 import Signup from "./pages/starter/Signup";
 import Login from "./pages/starter/Login";
-
+import Forgotpassword from "./pages/starter/ForgotPassword";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/signup" element={<Signup />}/>
+        <Route path="/register" element={<Signup />}/>
         <Route path="/login" element={<Login />}/>
+        <Route path="/forgotpassword" element={<Forgotpassword />}/>
       </Routes>
       <Footer />
     </Router>
