f63c587a34d7fb2de6f48198ebb8a8bf686c91aa
Rahmasbn <rahmasyabani37@gmail.com>
2022-01-27 19:16:28 +0700

    login & signUp implementation

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,25 @@
 import "./App.css";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; //Navigate
+import { PersistGate } from "redux-persist/es/integration/react";
+import { persistor } from "./redux/store";
+// import { connect } from "react-redux";
+
 import Footer from "./components/footer";
 import Signup from "./pages/starter/Signup";
 import Login from "./pages/starter/Login";
 
-
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/signup" element={<Signup />}/>
-        <Route path="/login" element={<Login />}/>
-      </Routes>
-      <Footer />
-    </Router>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </PersistGate>
   );
 }
 
--- src/pages/starter/Signup.js
+++ src/pages/starter/Signup.js
@@ -1,63 +1,92 @@
 import React from "react";
-import homeBg from "../../assets/loginbg.png";
-import Header from "../../components/header";
-import SignupCard from "../../components/starter";
-import googleIcon from '../../assets/google-icon.svg';
-import './style.css';
+import { useNavigate } from "react-router-dom";
 
-const Signup = () => {
+import homeBg from "../../assets/loginbg.png";
+import SignupCard from "../../components/starter";
+import googleIcon from "../../assets/google-icon.svg";
+
+import Header from "../../components/header";
+import { register } from "../../utils/https/auth";
+import "./style.css";
+
+const Signup = (props) => {
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const body = {
+      email: e.target.email.value,
+      password: e.target.password.value,
+      phone: e.target.phone.value,
+    };
+
+    register(body)
+      .then((res) => {
+        return navigate("/login", { replace: true });
+      })
+      .catch((err) => console.error(err));
+  };
   return (
     <>
       <main>
-      <section className="col-6 col-lg-6 signup-form-wrapper">
-      <Header />
-          <form className="signup-form">
-          <p className="signup-form-title">Sign Up</p>
-          <div className="mb-3">
-            <label className="form-label">Email address:</label>
-            <input
-              type="email"
-              className="form-control"
-              id="inputEmail1"
-              aria-describedby="emailHelp"
-            />
-          </div>
-          <div className="mb-3">
-            <label className="form-label">Password:</label>
-            <input
-              type="password"
-              className="form-control"
-              id="inputPassword1"
-            />
-          </div>
-          <div className="mb-3">
-            <label className="form-label">Phone Number:</label>
-            <input type="text" className="form-control" id="inputPhoneNumber" />
-          </div>
-          <div className="d-grid gap-2 col-12 mx-auto p-0" style={{ width: "100%" }}>
+        <section className="col-6 col-lg-6 signup-form-wrapper">
+          <Header />
+          <form className="signup-form" onSubmit={submitHandler}>
+            <p className="signup-form-title">Sign Up</p>
+            <div className="mb-3">
+              <label className="form-label">Email address:</label>
+              <input
+                type="email"
+                className="form-control"
+                id="inputEmail1"
+                name="email"
+                aria-describedby="emailHelp"
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Password:</label>
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                id="inputPassword1"
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Phone Number:</label>
+              <input
+                type="number"
+                className="form-control"
+                name="phone"
+                id="inputPhoneNumber"
+              />
+            </div>
             <div
-              className="btn btn-warning btn-signup"
+              className="d-grid gap-2 col-12 mx-auto p-0"
               style={{ width: "100%" }}
-              type="button"
             >
-              Signup
+              <button
+                className="btn btn-warning btn-signup"
+                style={{ width: "100%" }}
+                type="submit"
+              >
+                Signup
+              </button>
+              <div
+                className="btn btn-light btn-signup btn-signup-custom"
+                style={{ width: "100%" }}
+                type="button"
+              >
+                <img src={googleIcon} alt="logo" width="20" height="20" />
+                Signup with Google
+              </div>
             </div>
-            <div
-              className="btn btn-light btn-signup btn-signup-custom"
-              style={{ width: "100%" }}
-              type="button"
-            ><img src={googleIcon} alt="logo" width="20" height="20" />
-              Signup with Google
-            </div>
-          </div>
-        </form>
-      </section>
+          </form>
+        </section>
 
-      <aside className="col-6 col-lg-6 signup-img-wrapper">
-        <img src={homeBg} className="img-fluid" alt="Responsive" />
-      </aside>
-
-     
+        <aside className="col-6 col-lg-6 signup-img-wrapper">
+          <img src={homeBg} className="img-fluid" alt="Responsive" />
+        </aside>
       </main>
       <SignupCard />
     </>
