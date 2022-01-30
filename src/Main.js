c1be9ea547a451493aceeedd71b10d7292854011
special-snowflake <fzn475@gmail.com>
2022-01-31 01:50:09 +0700

    get product list, style needs to fix

--- src/Main.js
+++ src/Main.js
@@ -1,29 +1,38 @@
-import "./App.css";
+import './App.css';
 
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"; //Navigate
-import { PersistGate } from "redux-persist/es/integration/react";
-import { persistor } from "./redux/store";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom'; //Navigate
+import {PersistGate} from 'redux-persist/es/integration/react';
+import {persistor} from './redux/store';
 // import { connect } from "react-redux";
 // import { ToastContainer } from "react-toastify";
 
 // import Footer from './components/footer';
 // import Forgotpassword from './pages/starter/ForgotPassword.js';
-import Product from "./pages/product/Product";
-import Home from "./pages/main/Home";
-import ProductList from "./pages/productlist/productlist";
-import Profile from "./pages/profile/profile";
-import InvalidRoute from "./pages/redirects/InvalidRoute.js";
-import Footer from "./components/Footer";
-import Signup from "./pages/starter/Signup";
-import Login from "./pages/starter/Login";
-import Forgotpassword from "./pages/starter/ForgotPassword";
-import Chat from "./pages/Chat/Chat";
-import RoomChat from "./pages/Chat/RoomChat";
+import Product from './pages/product/Product';
+import Home from './pages/main/Home';
+import ProductList from './pages/productlist/productlist';
+import Profile from './pages/profile/profile';
+import InvalidRoute from './pages/redirects/InvalidRoute.js';
+import Footer from './components/Footer';
+import Signup from './pages/starter/Signup';
+import Login from './pages/starter/Login';
+import Forgotpassword from './pages/starter/ForgotPassword';
+import Chat from './pages/Chat/Chat';
+import RoomChat from './pages/Chat/RoomChat';
 import Addproduct from './pages/admin/product/AddProduct';
 import Editproduct from './pages/admin/product/EditProduct';
 import Payment from './pages/payment';
 import History from './pages/history';
-
+import ProductFavourite from './components/ProductFavourite';
+import ProductAddOn from './components/ProductAddOn';
+import ProductCoffee from './components/ProductCoffee';
+import ProductNonCoffee from './components/ProductNonCoffee';
+import ProductFoods from './components/ProductFoods';
 
 function App() {
   return (
@@ -32,19 +41,25 @@ function App() {
         <Routes>
           <Route path='404' element={<InvalidRoute />} />
           <Route path='*' exact element={<Navigate to='404' />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgotpassword" element={<Forgotpassword />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/room-chat" element={<RoomChat />} />
-          <Route path="/products" element={<Product />} />
-          <Route path="/product/detail" element={<ProductList />} />
-          <Route path="/add"  element={<Addproduct />} />
-          <Route path="/edit"  element={<Editproduct />} />
-          <Route path="/payment" element={<Payment />} />
-          <Route path="/history" element={<History />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/forgotpassword' element={<Forgotpassword />} />
+          <Route path='profile' element={<Profile />} />
+          <Route path='/chat' element={<Chat />} />
+          <Route path='/room-chat' element={<RoomChat />} />
+          <Route path='products' element={<Product />}>
+            <Route path='favourite' element={<ProductFavourite />} />
+            <Route path='coffee' element={<ProductCoffee />} />
+            <Route path='noncoffee' element={<ProductNonCoffee />} />
+            <Route path='foods' element={<ProductFoods />} />
+            <Route path='addon' element={<ProductAddOn />} />
+          </Route>
+          <Route path='/product/detail' element={<ProductList />} />
+          <Route path='/add' element={<Addproduct />} />
+          <Route path='/edit' element={<Editproduct />} />
+          <Route path='/payment' element={<Payment />} />
+          <Route path='/history' element={<History />} />
           {/* 
     <Router>
       <Routes>
---
+++ src/components/ProductAddOn/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {searchList} from '../../utils/https/products';
+import LoadingComponent from '../LoadingComponent';
+import ProductCard from '../ProductCard';
+
+class ProductAddOn extends React.Component {
+  state = {
+    isSuccess: false,
+    dataProduct: null,
+  };
+
+  componentDidMount() {
+    const filter = '?keyword=&orderBy=date&category=4&sort=desc';
+    searchList(filter)
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          isSuccess: true,
+          dataProduct: response.data.result,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  render() {
+    const {isSuccess, dataProduct} = this.state;
+    return (
+      <>
+        {isSuccess ? (
+          <div className='container product-content-wrapper'>
+            <ProductCard dataProduct={dataProduct} />
+          </div>
+        ) : (
+          <LoadingComponent />
+        )}
+      </>
+    );
+  }
+}
+
+export default ProductAddOn;
