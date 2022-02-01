8189160c983e73ee0c98e9b895cea2e9bb2c9068
Mahendraiqbal24 <mahendraiqbal42@gmail.com>
2022-02-01 21:23:52 +0700

    card product

--- src/components/historyCard/index.js
+++ src/components/historyCard/index.js
@@ -6,10 +6,10 @@ import imageVegie from "../../assets/vegie.png";
 function CardHistory() {
   return (
     <>
-      <div className="row col-12">
+      <div className="row col-12 w-100">
         <div className="col-md-4 card-history">
-          <div className="row col-md-11 card-history-cek">
-            <div className="row">
+          <div className="row col-md-12 card-history-cek w-100">
+            <div className="row w-100">
               <div className="col col-md-4">
                 <img
                   src={imageVegie}
@@ -18,13 +18,13 @@ function CardHistory() {
                 />
               </div>
               <div className="col col-md-8">
-                <p className="brand-history">Veggie tomato mix</p>
+                <p className="brand-history mt-2">Veggie tomato mix</p>
                 <p className="priced-history">IDR 34.000</p>
-                <div className="row">
+                <div className="row w-100">
                   <div className="col col-md-8 delivered-history">
                     Delivered
                   </div>
-                  <div className="col col-md-4">
+                  <div className="col col-md-4 checkbox-history">
                     <input type="checkbox" className="check-history-select" />
                   </div>
                 </div>
@@ -33,8 +33,8 @@ function CardHistory() {
           </div>
         </div>
         <div className="col-md-4 card-history">
-          <div className="row col-md-11 card-history-cek">
-            <div className="row">
+          <div className="row col-md-12 w-100 card-history-cek">
+            <div className="row w-100">
               <div className="col col-md-4">
                 <img
                   src={imageVegie}
@@ -43,13 +43,13 @@ function CardHistory() {
                 />
               </div>
               <div className="col col-md-8">
-                <p className="brand-history">Veggie tomato mix</p>
+                <p className="brand-history mt-2">Veggie tomato mix</p>
                 <p className="priced-history">IDR 34.000</p>
-                <div className="row">
+                <div className="row w-100">
                   <div className="col col-md-8 delivered-history">
                     Delivered
                   </div>
-                  <div className="col col-md-4">
+                  <div className="col col-md-4 checkbox-history">
                     <input type="checkbox" className="check-history-select" />
                   </div>
                 </div>
@@ -58,8 +58,8 @@ function CardHistory() {
           </div>
         </div>
         <div className="col-md-4 card-history">
-          <div className="row col-md-11 card-history-cek">
-            <div className="row">
+          <div className="row col-md-12 w-100 card-history-cek">
+            <div className="row w-100">
               <div className="col col-md-4">
                 <img
                   src={imageVegie}
@@ -68,13 +68,13 @@ function CardHistory() {
                 />
               </div>
               <div className="col col-md-8">
-                <p className="brand-history">Veggie tomato mix</p>
+                <p className="brand-history mt-2">Veggie tomato mix</p>
                 <p className="priced-history">IDR 34.000</p>
-                <div className="row">
+                <div className="row w-100">
                   <div className="col col-md-8 delivered-history">
                     Delivered
                   </div>
-                  <div className="col col-md-4">
+                  <div className="col col-md-4 checkbox-history">
                     <input type="checkbox" className="check-history-select" />
                   </div>
                 </div>
