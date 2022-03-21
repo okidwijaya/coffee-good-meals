1d010f3ae8fd5eeca90623536f8020d97788b831
Rahmasbn <rahmasyabani37@gmail.com>
2022-03-22 02:16:05 +0700

    add transaction

--- src/pages/starter/forgotpasswordAdv/VerifyOtp.js
+++ src/pages/starter/forgotpasswordAdv/VerifyOtp.js
@@ -32,10 +32,10 @@ function Verifyotp() {
   const [minute, setMinute] = useState(0);
 
   const secondsToTime = (secs) => {
-    var divisor_for_minutes = secs % (60 * 60);
-    var minutes = Math.floor(divisor_for_minutes / 60);
-    var divisor_for_seconds = divisor_for_minutes % 60;
-    var seconds = Math.ceil(divisor_for_seconds);
+    let divisor_for_minutes = secs % (60 * 60);
+    let minutes = Math.floor(divisor_for_minutes / 60);
+    let divisor_for_seconds = divisor_for_minutes % 60;
+    let seconds = Math.ceil(divisor_for_seconds);
     return {
       m: minutes,
       s: seconds,
